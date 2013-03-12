370556b833d1fd1b9c84bcc1f68ff01044b3dbb7
Gayle Bender <gayle@epiphanydevs.com>
2013-03-12 17:29:14 -0400

    basic sinatra app framework

---
+++ README.md
@@ -0,0 +1,9 @@
+Install Ruby version 1.9.3
+
+"gem install bundler"
+
+"bundle install"
+
+"ruby -rubygems myapp.rb"
+
+In a browser go to "http://localhost:4567"
---
+++ myapp.rb
@@ -0,0 +1,60 @@
+require 'sinatra'
+require 'json'
+
+get '/show' do
+#  @data = get '/data'  <-- that didn't work.
+  
+  @data =   [{
+                 :ident => "12345",
+                 :recording_time => "02/21/2013",
+                 :duration => 123,
+                 :gate => "Billing",
+                 :agent_id => "22111",
+                 :qa_score => 45,
+                 :qa_poss_score => 50,
+                 :survey_score => 10,
+                 :survey_poss_score => 10,
+             },
+             {
+                 :ident => "12346",
+                 :recording_time => "02/21/2013",
+                 :duration => 345,
+                 :gate => "Billing",
+                 :agent_id => "21333",
+                 :qa_score => 49,
+                 :qa_poss_score => 50,
+                 :survey_score => 9,
+                 :survey_poss_score => 10,
+             }].to_json
+
+      erb :show, :locals => {:data => @data}
+end
+
+
+
+get '/data' do
+  content_type :json
+
+  [{
+       :ident => "12345",
+       :recording_time => "02/21/2013",
+       :duration => 123,
+       :gate => "Billing",
+       :agent_id => "22111",
+       :qa_score => 45,
+       :qa_poss_score => 50,
+       :survey_score => 10,
+       :survey_poss_score => 10,
+   },
+   {
+       :ident => "12346",
+       :recording_time => "02/21/2013",
+       :duration => 345,
+       :gate => "Billing",
+       :agent_id => "21333",
+       :qa_score => 49,
+       :qa_poss_score => 50,
+       :survey_score => 9,
+       :survey_poss_score => 10,
+   }].to_json
+end
